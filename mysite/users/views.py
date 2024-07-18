56d926968d85222627b6209d0006ff99efc86030
Cr1mson <o_devon2@yahoo.com>
2024-07-18 16:06:44 -0400

    Added a validation check to my form

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def register_view(request):
-    form = UserCreationForm()
+    #Checks if the form has been submitted with information
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        # If the form is valid, it saves
+        if form.is_valid():
+            form.save()
+            return redirect("posts:list")
+    else:
+        form = UserCreationForm()
     return render(request, "users/register.html", { "form": form })
