974feb009126ce81de0f1ba24a9223662c80168c
Cr1mson <o_devon2@yahoo.com>
2024-07-18 15:49:12 -0400

    Fixed a misplaced "

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
-def register(request):
+def register_view(request):
     form = UserCreationForm()
-    return render(request, 'users/users_register.html', { "form: form"})
+    return render(request, "users/register.html", { "form": form })
