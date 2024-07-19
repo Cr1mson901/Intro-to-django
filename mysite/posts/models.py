0aafea8a9884f28951ad85412bda7315cee9f7f7
Cr1mson <o_devon2@yahoo.com>
2024-07-19 14:02:12 -0400

    Added author to the post class and enabled New posts to be generated

--- mysite/posts/models.py
+++ mysite/posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
@@ -9,6 +10,8 @@ class Post(models.Model):
     #ImageField requires Pillow to be installed
     banner = models.ImageField(default='fallback.png', blank=True)
     # py manage.py makemigrations is required when updating a model. Must migrate after
+    #This is an example of a many to one relational database. Cascade means delete all the posts from a user if a user is deleted
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     
     def __str__(self):
         return self.title
--- mysite/posts/views.py
+++ mysite/posts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
 from django.contrib.auth.decorators import login_required
+from . import forms
 
 
 # Create your views here.
@@ -16,4 +17,16 @@ def post_page(request, slug):
 #Decorator checks if a user is logged in. If not then it redirects them to /users/login/
 @login_required(login_url="/users/login/")
 def post_new(request):
-    return render(request, 'posts/post_new.html')
+    if request.method == "POST":
+        # Need .FILES because we are sending an image file too
+        form = forms.CreatePost(request.POST, request.FILES)
+        if form.is_valid():
+            newpost = form.save(commit=False)
+            #Gets the user that is logged in and adds it to the post object
+            newpost.author = request.user
+            # Saves the post
+            newpost.save()
+            return redirect("posts:list")
+    else:        
+        form = forms.CreatePost()
+    return render(request, 'posts/post_new.html', {'form': form})
