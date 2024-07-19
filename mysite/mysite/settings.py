1c0718f7a0e0c9a2419555919b411ec64cecb463
Cr1mson <o_devon2@yahoo.com>
2024-07-19 14:24:05 -0400

    Modernized some elements and collected assets into one place

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$u1y!um*4aegm(p7+0*dox4x^j04=$8_08ar%)sm4hkf)+$iz6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -118,15 +117,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
-#For implementing css
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-#For implementing images. Media gets created when an image is uploaded
 MEDIA_URL = 'media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#Run collectassets
+STATIC_ROOT = BASE_DIR / 'assets'
+#For implementing images. Media gets created when an image is uploaded
+MEDIA_ROOT = BASE_DIR / 'media'
+#For implementing css
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
