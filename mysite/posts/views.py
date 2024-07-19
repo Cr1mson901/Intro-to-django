a270cc299b8d95bfb85777b17b6d15527be3ed82
Cr1mson <o_devon2@yahoo.com>
2024-07-19 13:17:47 -0400

    Added Logout button, login protected page. And user auth

--- mysite/posts/views.py
+++ mysite/posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -11,3 +12,8 @@ def posts_lists(request):
 def post_page(request, slug):
     post = Post.objects.get(slug=slug)
     return render(request, 'posts/post_page.html', {'post':post})
+
+#Decorator checks if a user is logged in. If not then it redirects them to /users/login/
+@login_required(login_url="/users/login/")
+def post_new(request):
+    return render(request, 'posts/post_new.html')
--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 
 # Create your views here.
 def register_view(request):
@@ -22,7 +22,16 @@ def login_view(request):
         if form.is_valid():
             #Logs in if valid
             login(request, form.get_user())
-            return redirect("posts:list")
+            #Checks if there is a next parameter in the url. If so, redirect to the value of the next
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+            else:
+                return redirect("posts:list")
     else:
         form = AuthenticationForm()
     return render(request, "users/login.html", {"form": form})
+
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect("posts:list")
